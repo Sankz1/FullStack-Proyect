af160ecba918229de8d3fa4f9e1d4469ccd7f588
Edwin <e.devia.b1306@gmail.com>
2024-11-05 09:15:06 -0500

    Encrypted Password

--- src/controllers/Authenticate_Controller.js
+++ src/controllers/Authenticate_Controller.js
@@ -1,11 +1,13 @@
 //Controllers of routes
 import User from "../models/userModel.js";
+import crypt from 'bcryptjs'
 export const  register = async (request, resp) => {
     const {contactEmail, username, password} = request.body
     try {
+      const passwordEncrypted = await crypt.hash(password, 10)
         const newUser = new User({
             username,
-            password,
+            password: passwordEncrypted,
             contactEmail
         })
 const userSave =    await newUser.save()
