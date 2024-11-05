1e541f98fdf202ef1ada4392fc6e932f168488ea
Edwin <e.devia.b1306@gmail.com>
2024-11-05 12:17:34 -0500

    JWT in cookie adjustment

--- src/controllers/Authenticate_Controller.js
+++ src/controllers/Authenticate_Controller.js
@@ -1,21 +1,51 @@
 //Controllers of routes
 import User from "../models/userModel.js";
-import crypt from 'bcryptjs'
-export const  register = async (request, resp) => {
-    const {contactEmail, username, password} = request.body
-    try {
-      const passwordEncrypted = await crypt.hash(password, 10)
-        const newUser = new User({
-            username,
-            password: passwordEncrypted,
-            contactEmail
-        })
-const userSave =    await newUser.save()
-        resp.send(userSave)
-    } catch(error){
-        console.log(error)
-    }
+import crypt from "bcryptjs";
+import JWT from "jsonwebtoken";
 
+export const register = async (request, resp) => {
+  const { contactEmail, username, password } = request.body;
+  try {
+    const passwordEncrypted = await crypt.hash(password, 10);
 
-}
-export const login = (request,resp) => {resp.send("logged")}
+    const newUser = new User({
+      username,
+      password: passwordEncrypted,
+      contactEmail,
+    });
+
+    const userSave = await newUser.save();
+    JWT.sign(
+      {
+        id: userSave._id,
+      },
+      "test123",
+      {
+        expiresIn: "1d",
+      },
+      (err, token) => {
+        if (err) console.log(err);
+        resp.cookie("token", token);
+        resp.statusCode = 200;
+        resp.json({
+          message: "User created successfully",
+        });
+      },
+    );
+
+    /*    resp.json({
+                                                          id: userSave._id,
+                                                          username: userSave.username,
+                                                          email: userSave.contactEmail,
+                                                        });*/
+  } catch (error) {
+    console.log(error);
+    resp.statusCode = 400;
+    resp.json({
+      message: "The user is already registered",
+    });
+  }
+};
+export const login = (request, resp) => {
+  resp.send("logged");
+};
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,21 +1,24 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 // Schema to create a user
-const userSchema = new  mongoose.Schema({
- username:{
-     type: String,
-     required : true, //Requirement
-     trim : true      //Delete spaces
- },
- contactEmail:{
-     type: String,
-     required :true,
-     unique: true,  //Unique validation
- },
- password:{
-     type: String,
-     required : true,
- }},
-    {
-        timestamps:true,
-    })
-export default mongoose.model('Users',userSchema) // Metohd to use the data
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true, //Requirement
+      trim: true, //Delete spaces
+    },
+    contactEmail: {
+      type: String,
+      required: true,
+      unique: true, //Unique validation
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+export default mongoose.model("Users", userSchema); // Metohd to use the data
