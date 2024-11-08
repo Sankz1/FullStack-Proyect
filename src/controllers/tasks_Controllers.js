1ac824d7fcdb8030f97f14ed7d88b67d6a13511e
Edwin <e.devia.b1306@gmail.com>
2024-11-08 16:56:52 -0500

    Crud last changes, task already config

--- src/controllers/tasks_Controllers.js
+++ src/controllers/tasks_Controllers.js
@@ -1,5 +1,46 @@
-export const postTask= async (request,response)=> {}
-export const getTasks= async (request,response)=> {}
-export const getOneTask= async (request,response)=> {}
-export const putTask= async (request,response)=> {}
-export const deleteTask= async (request,response)=> {}
+import Task from "../models/taskModels.js";
+
+export const postTask= async (request,response)=> {
+    const {title,description,date} = request.body;
+ const newTask = new Task({
+        title,
+        description,
+        date,
+        user:request.decodedUser.id
+    })
+    const taskSaved = await newTask.save();
+    response.statusCode = 200;
+    response.json(taskSaved)
+}
+
+export const getTasks= async (request,response)=> {
+    const tasks = await Task.find({
+        user: request.decodedUser.id
+    }).populate('user')
+    response.json(tasks);
+}
+
+export const getOneTask= async (request,response)=> {
+    const task = await Task.findById(request.params.id).populate('user')
+    if (!task) {
+        return response.status(404).json({message: "Task not found"})
+    }
+
+    response.json(task)
+}
+
+export const putTask= async (request,response)=> {
+    const task = await Task.findByIdAndUpdate(request.params.id,request.body, {new:true})
+    if (!task) {
+        return response.status(404).json({message: "Task not found"})
+    }
+    response.json(task)
+}
+
+export const deleteTask= async (request,response)=> {
+    const task = await Task.findByIdAndDelete(request.params.id)
+    if (!task) {
+        return response.status(404).json({message: "Task not found"})
+    }
+    response.sendStatus(204)
+}
